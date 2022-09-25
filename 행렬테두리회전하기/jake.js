7dfca5ba347273e5d538501f7fcd1a972624b441
Jake <tkwk1205@naver.com>
2022-09-26 00:15:30 +0900

    로직 변경

--- 행렬테두리회전하기/jake.js
+++ 행렬테두리회전하기/jake.js
@@ -3,39 +3,42 @@ function solution(rows, columns, queries) {
   let answer = [];
 
   for (let q of queries) {
-    const [x1, y1, x2, y2] = q;
+    let [x1, y1, x2, y2] = q;
+    x1--;
+    y1--;
+    x2--;
+    y2--;
 
-    let beforeValue = arr[x1 - 1][y1 - 1]; // 이전 행렬 위치의 값
-    let constValue = arr[x1 - 1][y1 - 1]; // 현재 행렬 위치의 값
+    let beforeValue = arr[x1][y1]; // 이전 행렬 위치의 값
+    let constValue = arr[x1][y1]; // 현재 행렬 위치의 값
     let minValue = constValue; // 최소값
 
     // ➡️ 이동하며 값 변경
-    for (let y = y1; y < y2; y++) {
-      constValue = arr[x1 - 1][y]; // 현재 행렬 위치의 값 저장
-      arr[x1 - 1][y] = beforeValue; // 현재 행렬 위치의 값을 이전 행렬 위치의 값으로 변경
+    for (let y = y1 + 1; y <= y2; y++) {
+      constValue = arr[x1][y]; // 현재 행렬 위치의 값 저장
+      arr[x1][y] = beforeValue; // 현재 행렬 위치의 값을 이전 행렬 위치의 값으로 변경
       beforeValue = constValue; // 이전 값에 현재 값으로 변경하여 저장
       minValue = Math.min(minValue, constValue); // 가장 작은 값과 현재 값 비교하여 작은 값 저장
     }
     // ⬇️ 이동하며 값 변경
-    for (let x = x1; x < x2; x++) {
-      constValue = arr[x][y2 - 1];
-      arr[x][y2 - 1] = beforeValue;
+    for (let x = x1 + 1; x <= x2; x++) {
+      constValue = arr[x][y2];
+      arr[x][y2] = beforeValue;
       beforeValue = constValue;
       minValue = Math.min(minValue, constValue);
     }
     // ⬅️ 이동하며 값 변경
-    for (let y = y2 - 2; y > y1 - 2; y--) {
-      constValue = arr[x2 - 1][y];
-      arr[x2 - 1][y] = beforeValue;
+    for (let y = y2 - 1; y >= y1; y--) {
+      constValue = arr[x2][y];
+      arr[x2][y] = beforeValue;
       beforeValue = constValue;
       minValue = Math.min(minValue, constValue);
     }
     // ⬆️ 이동하며 값 변경
-    for (let x = x2 - 2; x > x1 - 2; x--) {
-      constValue = arr[x][y1 - 1];
-      arr[x][y1 - 1] = beforeValue;
+    for (let x = x2 - 1; x >= x1; x--) {
+      constValue = arr[x][y1];
+      arr[x][y1] = beforeValue;
       beforeValue = constValue;
-
       minValue = Math.min(minValue, constValue);
     }
 
